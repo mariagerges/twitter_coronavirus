b7caf5bba45e77a094c009501ab6a6fdbec619a3
Maria Gerges <mgerges23@cmc.edu>
2023-02-21 20:34:13 -0800

    all images

--- src/visualize.py
+++ src/visualize.py
@@ -12,6 +12,9 @@ args = parser.parse_args()
 import os
 import json
 from collections import Counter,defaultdict
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
 
 # open the input path
 with open(args.input_path) as f:
@@ -26,3 +29,20 @@ if args.percent:
 items = sorted(counts[args.key].items(), key=lambda item: (item[1],item[0]), reverse=True)
 for k,v in items:
     print(k,':',v)
+
+# Create bar plot
+for k,v in items[:10]:
+    plt.bar(k, v)
+
+# Set plot title and axis labels
+plt.title('Top 10 countries for 코로나바이러스 Hashtag')
+plt.xlabel('Country')
+plt.ylabel('Hashtag')
+
+# Save plot as PNG file
+plt.savefig('country_other.png')
+
+
+# Show plot
+plt.show()
+
